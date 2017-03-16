89e9e773f916705107c0f7ebd55b4f360274b890
plant <dady_ocat@hotmail.com>
2017-03-16 18:58:53 +0700

    exampes

---
+++ examples/switch/switch.ino
@@ -0,0 +1,51 @@
+include <ESP8266WiFi.h>
+
+#include <IoTplant_DigitalPin.h>
+#include <IoTplant_WIFI.h>
+
+
+//หลอดไฟ  แสดงสถานนะ  WIFI  
+const int LED_Red  =  D7;
+const int LED_Green = D8; 
+ 
+//หลอดไฟแสดงสถานะการส่งข้อมูล
+const int LED_REQUEST = D6;
+//หลอดไฟแสดงสถานะ
+DigitalPin WIFI_GREEN(LED_Green);
+DigitalPin WIFI_RED(LED_Red);
+
+
+//gpio
+const int gpio = D2; 
+
+//wifi 
+char ssid[] = "kid";
+char pass[] = "hp640554";
+
+
+
+
+void setup() {
+  //led bein
+  WIFI_GREEN.begin();
+  WIFI_RED.begin();
+
+  set_req(LED_REQUEST);
+  Serial.begin(115200); 
+
+  WIFI_RED.on();
+  if(wifi(ssid,pass)){
+      WIFI_GREEN.on();
+      WIFI_RED.off();        
+  }else{
+      WIFI_GREEN.off(); 
+      WIFI_RED.on();
+  }
+    
+}
+
+void loop() {
+   api_repost_switch(gpio);
+   api_send("xxxxx"); //api-key
+
+}
